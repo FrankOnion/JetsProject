05fdf472c518883560531980b38f11da9d932902
FrankOnion <148408719+FrankOnion@users.noreply.github.com>
2023-12-03 22:40:20 -0500

    Add README.md w/ project breakdown

--- src/com/skilldistillery/jets/JetsApplication.java
+++ src/com/skilldistillery/jets/JetsApplication.java
@@ -1,6 +1,8 @@
 package com.skilldistillery.jets;
 
 public class JetsApplication {
+	
+	private AirField airField;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
